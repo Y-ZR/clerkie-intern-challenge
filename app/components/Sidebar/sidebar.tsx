fba295e11f416eeb0523420c59c71ba35bde1e85
ZR <zhongrong123@gmail.com>
2023-10-25 04:49:16 +0800

    testing Vercel build

--- app/components/Sidebar/sidebar.tsx
+++ app/components/Sidebar/sidebar.tsx
@@ -2,8 +2,8 @@
 import React from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import Icon from '../Icon/Icon';
 import styles from './sidebar.module.css';
+import Icon from '../Icon/Icon';
 
 // Define the interface for NavLink
 export interface NavLink {
