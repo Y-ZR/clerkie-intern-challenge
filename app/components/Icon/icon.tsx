7a92e55ff1e04344ad1b47a24088227e91c6b201
ZR <zhongrong123@gmail.com>
2023-10-25 04:00:01 +0800

    css fixes for sidebar and homepage

--- app/components/Icon/icon.tsx
+++ app/components/Icon/icon.tsx
@@ -1,25 +1,25 @@
 import Image from 'next/image';
-import { FC } from 'react';
+import React from 'react';
 
-// Define props interface for the Icon component
+// Define the props interface
 interface IconProps {
-  src: string;      // Source for the image
-  alt: string;      // Alt text for the image
-  dimensions: number; // Dimensions for the image (width and height)
+  title: string;
+  dimensions: number;
+  spaced: boolean;
 }
 
-// Create a functional component named Icon that accepts props of type IconProps
-const Icon: FC<IconProps> = ({ src, alt, dimensions }) => {
+// Create a functional component called Icon
+const Icon: React.FC<IconProps> = ({ title, dimensions, spaced }) => {
+  // Construct the JSX for the icon
   return (
-    // Render an Image component with properties provided in the props
     <Image
-      src={src}        // Image source
-      alt={alt}        // Alt text for the image
-      width={dimensions} // Width of the image
-      height={dimensions} // Height of the image
-      style={{ marginLeft: '10px', marginRight: '10px' }} // Inline style for the image
+      src={`/${title}.svg`}
+      alt={title}
+      width={dimensions}
+      height={dimensions}
+      style={spaced ? { marginLeft: '10px', marginRight: '10px' } : {}}
     />
   );
-}
+};
 
 export default Icon;
--- app/components/Sidebar/sidebar.tsx
+++ app/components/Sidebar/sidebar.tsx
@@ -1,44 +1,45 @@
 "use client"
-
+import React from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { FC } from 'react';
 import Icon from '../Icon/icon';
 import styles from './sidebar.module.css';
 
-// Define an interface for navigation links
+// Define the interface for NavLink
 export interface NavLink {
   title: string;
   path: string;
 }
 
-// Create an array of navigation links
+// Define an array of navigation links
 export const navLinks: NavLink[] = [
   { title: 'Home', path: '/' },
-  { title: 'Friends', path: '/friends' }
+  { title: 'Friends', path: '/friends' },
 ];
 
-// Define the Sidebar component as a functional component
-const Sidebar: FC = () => {
+// Create the SideBar functional component
+const SideBar: React.FC = () => {
   // Get the current pathname using usePathname hook
   const pathname = usePathname();
 
+  // Construct the JSX for the SideBar
   return (
     <div className={styles.sidebar}>
       <div className={styles.sidebarHeader}>
-        {/* Display the Clerkie Icon */}
-        <Icon src="/Clerkie.svg" alt="clerkie-icon" dimensions={20} />
+        {/* Render the Icon component with specific props */}
+        <Icon title="Clerkie" dimensions={20} spaced={true} />
         <span>Clerkie Challenge</span>
       </div>
-
-      {/* Map over the navigation links and create link elements */}
       {navLinks.map((link) => (
+        // Create links using the Link component
         <Link href={link.path} key={link.path}>
           <div
-            className={`${styles.link} ${pathname === link.path ? styles.activeLink : ''}`}
+            className={`${styles.link} ${
+              pathname === link.path ? styles.activeLink : ''
+            }`}
           >
-            {/* Display the Home Icon and link title */}
-            <Icon src="/Home.svg" alt="home-icon" dimensions={24} />
+            {/* Render the Icon component for each link with specific props */}
+            <Icon title={link.title} dimensions={24} spaced={true} />
             <span>{link.title}</span>
           </div>
         </Link>
@@ -47,4 +48,4 @@ const Sidebar: FC = () => {
   );
 };
 
-export default Sidebar;
+export default SideBar;
